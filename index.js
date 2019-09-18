f8e667c3abbe08f32abfc0ab42e7e7670122132c
victor-mthemba <victormthemba@gmail.com>
2019-09-18 12:42:49 +0200

    my second commit

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const exphbs  = require('express-handlebars');
+const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const SettingsBill = require('./settings-bill');
+var moment = require('moment');
+moment().format();
 
 
 const app = express();
@@ -9,8 +11,8 @@ const settingsBill = SettingsBill();
 
 const handlebarSetup = exphbs({
     partialsDir: "./views/partials",
-    viewPath:  './views',
-    layoutsDir : './views/layouts'
+    viewPath: './views',
+    layoutsDir: './views/layouts'
 });
 
 app.engine('handlebars', handlebarSetup);
@@ -19,56 +21,70 @@ app.set('view engine', 'handlebars');
 app.use(express.static('public'));
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
 
 // parse application/json
 app.use(bodyParser.json())
 
-app.get('/', function(req, res ) {
+app.get('/', function (req, res) {
     res.render('index', {
         settings: settingsBill.getSettings(),
         totals: settingsBill.totals(),
         colorChange: settingsBill.colorChanger()
-    }); 
+    });
 });
 
-app.post('/settings', function(req, res) {
-    
+app.post('/settings', function (req, res) {
+
     settingsBill.setSettings({
         callCost: req.body.callCost,
         smsCost: req.body.smsCost,
         warningLevel: req.body.warningLevel,
         criticalLevel: req.body.criticalLevel,
     });
-    
+
     res.redirect('/');
 
 });
 
-app.post('/action', function(req, res) {
+app.post('/action', function (req, res) {
 
     // capture the call type to add
     console.log(req.body.actionType);
-    
+
     settingsBill.recordAction(req.body.actionType);
     res.redirect('/');
 });
 
-app.get('/actions', function(req, res) {
-    res.render('actions', {actions: settingsBill.actions()});
+app.get('/actions', function (req, res) {
+    var time = settingsBill.actions()
+    for (const iterator of time) {
+        iterator.ago = moment(iterator.timestamp).fromNow()
+    }
+    res.render('actions', {
+        actions: time
+    });
 });
 
-app.get('/actions/:actionType', function(req, res) {
+app.get('/actions/:actionType', function (req, res) {
     const actionType = req.params.actionType;
-    res.render('actions', {actions: settingsBill.actionsFor(actionType)});
+    var time = settingsBill.actionsFor(actionType)
+    for (const iterator of time) {
+        iterator.ago = moment(iterator.timestamp).fromNow()
+    }
+    res.render('actions', {
+        actions: time
+    });
 });
 
-app.post('/action:type', function(req, res) {
+app.post('/action:type', function (req, res) {
 
 });
 
 const PORT = process.env.PORT || 3811;
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("App started at port: ", PORT)
 });
